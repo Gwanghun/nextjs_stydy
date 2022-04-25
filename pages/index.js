8ab1fa082237afd4d8d366cd426e59dc265269dc
hooeni <hooeni@gmail.com>
2022-04-26 00:06:32 +0900

    페이지로 넘겨주는 파라미터 제거

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,7 @@ import Seo from "../components/Seo";
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: "/movies/[id]",
-        query: { title }
-      },
-      `/movies/${id}`   // push 의 as 속성
-    )
+    router.push(`/movies/${title}/${id}`)
   }
   return (
     <div className="container">
@@ -24,13 +18,7 @@ export default function Home({ results }) {
         >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>
-            <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: { title: movie.original_title }
-              }}
-              as={`/movies/${movie.id}`}
-            >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               {movie.original_title}
             </Link>
           </h4>
@@ -65,7 +53,7 @@ export default function Home({ results }) {
 }
 
 export async function getStaticProps() {
-  const { results } = await (await fetch(`http://localhost:3000//api/movies`)).json();
+  const { results } = await (await fetch(`http://localhost:3000/api/movies`)).json();
   return {
     props: {
       results
